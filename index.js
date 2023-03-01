e3e5ab3d4b4bd788ca01c88246e6f17ad9629a15
Googlepony <ponybitly@gmail.com>
2023-03-01 15:37:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,43 @@
+const express = require('express');
 const multer = require('multer');
 const tabula = require('tabula-js');
 const path = require('path');
 
-module.exports = (req, res) => {
-    if (req.method === 'GET') {
-        res.send('Get - Working');
-    } else if (req.method === 'POST') {
-        const upload = multer({
-            storage: multer.memoryStorage()
-        }).single('pdf');
-        upload(req, res, async (err) => {
-            if (err) {
-                res.status(500).send(`Error occurred while processing PDF: ${err.message}`);
-                return;
-            }
-            try {
-                const pdfBuffer = req.file.buffer;
-                const stream = tabula(pdfBuffer, { pages: "all" }, { area: "80, 30, 1080 , 810" }).streamCsv();
-                const fileStream = stream.fork();
-                let csvData = '';
-                fileStream.on('data', chunk => csvData += chunk);
-                fileStream.on('end', () => {
-                    res.setHeader('Content-Type', 'text/csv');
-                    res.setHeader('Content-Disposition', `attachment; filename=${path.parse(req.file.originalname).name}.csv`);
-                    res.send(csvData);
-                });
-            } catch (err) {
-                res.status(500).send(`Error occurred while processing PDF: ${err.message}`);
-            }
-        });
+const app = express();
+
+// configure multer for handling file upload
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './');
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
     }
-};
+});
+const upload = multer({ storage });
+
+app.get('/', (req, res) => {
+    res.send('Get - Working');
+});
+
+app.post('/api/convert', upload.single('pdf'), async (req, res) => {
+    try {
+        const pdfPath = req.file.path;
+        const stream = tabula(pdfPath, { pages: "all" }, { area: "80, 30, 1080 , 810" }).streamCsv();
+        const fileStream = stream.fork();
+        let csvData = '';
+        fileStream.on('data', chunk => csvData += chunk);
+        fileStream.on('end', () => {
+            res.setHeader('Content-Type', 'text/csv');
+            res.setHeader('Content-Disposition', `attachment; filename=${path.parse(req.file.originalname).name}.csv`);
+            res.send(csvData);
+        });
+    } catch (err) {
+        res.status(500).send(`Error occurred while processing PDF: ${err.message}`);
+    }
+});
+
+module.exports = app;
 
 
 
