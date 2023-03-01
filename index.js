8283dba220f1aad41510a588bfb7dcb9861e974b
Googlepony <ponybitly@gmail.com>
2023-03-01 15:09:35 +0530

    added

--- index.js
+++ index.js
@@ -38,7 +38,6 @@ app.post('/convert', upload.single('pdf'), async (req, res) => {
 });
 
 module.exports = app;
-
-// app.listen(process.env.PORT || 3000, () => {
-//     console.log(`Server listening on port ${process.env.PORT || 3000}`);
-// });
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server listening on port ${process.env.PORT || 3000}`);
+});
