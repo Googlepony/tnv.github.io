dbddfbae641f853a32197f4d782b5b5c69618d00
Googlepony <ponybitly@gmail.com>
2023-03-01 15:03:05 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,50 +1,44 @@
 const express = require('express');
+const multer = require('multer');
 const tabula = require('tabula-js');
 const path = require('path');
-const fs = require('fs');
 
 const app = express();
+
+// configure multer for handling file upload
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './uploads');
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    }
+});
+const upload = multer({ storage });
 
 app.get('/', (req, res) => {
     res.send('Get - Working');
 });
 
-app.post('/convert', async (req, res) => {
-    const pdfDir = './';
-
-    const pdfFiles = fs.readdirSync(pdfDir, { withFileTypes: true })
-        .filter(dirent => dirent.isFile() && dirent.name.endsWith('.pdf'))
-        .map(dirent => dirent.name);
-
-    const csvDataArray = [];
-    const promises = pdfFiles.map(async (pdfFile) => {
-        const pdfPath = path.join(pdfDir, pdfFile);
+app.post('/convert', upload.single('pdf'), async (req, res) => {
+    try {
+        const pdfPath = req.file.path;
         const stream = tabula(pdfPath, { pages: "all" }, { area: "80, 30, 1080 , 810" }).streamCsv();
         const fileStream = stream.fork();
-        await new Promise((resolve, reject) => {
-            let csvData = '';
-            fileStream.on('data', chunk => csvData += chunk);
-            fileStream.on('end', () => {
-                csvDataArray.push(csvData);
-                resolve();
-            });
-            fileStream.on('error', (err) => reject(err));
+        let csvData = '';
+        fileStream.on('data', chunk => csvData += chunk);
+        fileStream.on('end', () => {
+            res.setHeader('Content-Type', 'text/csv');
+            res.setHeader('Content-Disposition', `attachment; filename=${path.parse(req.file.originalname).name}.csv`);
+            res.send(csvData);
         });
-    });
-
-    try {
-        await Promise.all(promises);
-        const csvData = csvDataArray.join('');
-        res.setHeader('Content-Type', 'text/csv');
-        res.setHeader('Content-Disposition', 'attachment; filename=merged.csv');
-        console.log(csvData);
-        res.send(csvData);
     } catch (err) {
-        res.status(500).send(`Error occurred while processing PDFs: ${err.message}`);
+        res.status(500).send(`Error occurred while processing PDF: ${err.message}`);
     }
 });
 
 module.exports = app;
+
 // app.listen(process.env.PORT || 3000, () => {
 //     console.log(`Server listening on port ${process.env.PORT || 3000}`);
 // });
